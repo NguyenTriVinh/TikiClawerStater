abd65552054efcdb0ef9eaf0f8e9375f3f23a28c
QuachNgoc <quachbaongoc02@gmail.com>
2023-08-17 22:03:49 +0700

    homework

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from TikiClawer import TikiClawer
+from data import *
+import time 
 
-tiki_url = "https://tiki.vn"
+# Const
+tiki_url = "https://tiki.vn/search?q="
+
+keywords = "balo"
 
 tiki = TikiClawer()
 
-tiki.getUrl(tiki_url)
+tiki.getUrl(tiki_url + keywords)
+links = tiki.getProductLinks(productLinksXpath)
+print(links)
+
+total_products = []
+
+time.sleep(3)
+
+for link in links:
+    # Vào chi tiết sản phẩm
+    tiki.getUrl(link)
+    
+    # Lấy brand
+    tiki.getBrand(brand_xpath)
+    
+    # Lấy name
+    
+    # lấy price
+    
